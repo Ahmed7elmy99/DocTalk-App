363062e72adb55da4c01780891aecdd363ba8267
i seven <https://github.com/Elsayedkamel2>
2024-05-17 14:35:37 +0300

    handel dio helper

--- lib/app/utils/dio_helper.dart
+++ lib/app/utils/dio_helper.dart
@@ -1,47 +1,49 @@
 import 'package:dio/dio.dart';
 
+
 class DioHelper {
   static late Dio dio;
   static init() {
     dio = Dio(BaseOptions(
-        baseUrl: "",
-        receiveDataWhenStatusError: true));
+      baseUrl: "",
+      receiveDataWhenStatusError: true,
+    ));
   }
 
 //130.61.130.252/api/auth/
   static Future<Response> getData(
       {required String url,
-       Map<String, dynamic>?   queryParameters,
-      Map<String, dynamic>? data}) async {
-    dio.options.headers = {
-      "Content-Type": " application/json",
-      "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiaWF0IjoxNzEzNDUzMTYxLCJleHAiOjE3MTM0ODkxNjF9.9Qfp_ZX3l0gJGk0RAdtda1nrhkjWCE_zqtSRu43KrPs",
-    };
-    return await dio.get(
-        url, queryParameters: queryParameters, data: data,
-    );
+        Map<String, dynamic>? headers,
+        Map<String, dynamic>? queryParameters,
+        Map<String, dynamic>? data}) async {
+    //  dio.options.headers = { 'Authorization':  CashHelper.getString(key: "token")  };
+    return await dio.get(url, queryParameters: queryParameters, data: data,    options: Options(
+        headers: headers
+    ),);
   }
 
   static Future<Response> postData(
       {required String url,
-      Map<String, dynamic>? queryParameters,
-      required Map<String, dynamic> data}) async {
-    dio.options.headers = {
-      "Content-Type": "application/json",
-      "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiaWF0IjoxNzEzNDUzMTYxLCJleHAiOjE3MTM0ODkxNjF9.9Qfp_ZX3l0gJGk0RAdtda1nrhkjWCE_zqtSRu43KrPs",
-    };
-  dynamic   response = await dio.post(
+        Map<String, dynamic>? queryParameters,
+        Map<String, dynamic>? headers,
+        required Map<String, dynamic> data}) async {
+    // dio.options.headers = { 'Authorization':"Bearer ${ CashHelper.getString(key: "token")}" };
+    dynamic response = await dio.post(
       url,
       queryParameters: queryParameters,
       data: data,
+
+      options: Options(
+          headers: headers
+      ),
     );
     return response;
   }
 
   static Future<Response> patchData(
       {required String url,
-      Map<String, dynamic>? queryParameters,
-      required Map<String, dynamic> data}) async {
+        Map<String, dynamic>? queryParameters,
+        required Map<String, dynamic> data}) async {
     return await dio.patch(
       url,
       queryParameters: queryParameters,
