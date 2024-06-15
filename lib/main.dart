3956a21e98d660f23fe7538272e381ed0153d823
Ahmed7elmy99 <abozahraahmed94@gmail.comm>
2024-06-14 20:42:48 -0700

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,9 @@ import 'package:doc_talk/app/utils/dio_helper.dart';
 import 'package:doc_talk/features/auth_feature/presentation/cubit/auth_cubit.dart';
 import 'package:doc_talk/features/home_feature/presentation/screens/bottom_nav_bar.dart';
 
-<<<<<<< HEAD
 
 import 'package:doc_talk/features/levels_and_categories/presentation/cubit/levels_and-categories_cubit.dart';
 
-=======
->>>>>>> c4827c53ae93b65a9a1f680dbac2dfac087b5090
 import 'package:doc_talk/features/questionair_feature/cubit/survey_cubit.dart';
 import 'package:doc_talk/features/quiz/cubit/quiz_cubit.dart';
 import 'package:doc_talk/features/splash_and_onboarding_feature/presentation/screens/splash_screen.dart';
@@ -22,9 +19,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-import 'features/levels_and_categories/presentation/cubit/levels_and-categories_cubit.dart';
-import 'features/quiz/views/quiz_one.dart';
-
 //new
 //nwe2
 void main() async {
@@ -32,7 +26,6 @@ void main() async {
   DioHelper.init();
   Bloc.observer = MyBlocObserver();
   await CashHelper.init();
-  print( CashHelper.getString(key: "token"));
   runApp(
     MultiBlocProvider(
       providers: [
@@ -43,20 +36,12 @@ void main() async {
           create: (BuildContext context) => SurveyCubit(),
         ),
         BlocProvider(
-<<<<<<< HEAD
           create: (BuildContext context) => LevelsCubit()
             ..getCategoriesHomeData(context: context, levelId: 1),
         ),
         BlocProvider(
           create: (BuildContext context) => QuizCubit(),
         )
-=======
-          create: (BuildContext context) => QuizCubit(),
-        ),
-        BlocProvider(
-          create: (BuildContext context) => LevelsCubit(),
-        ),
->>>>>>> c4827c53ae93b65a9a1f680dbac2dfac087b5090
       ],
       child: const MyApp(),
     ),
